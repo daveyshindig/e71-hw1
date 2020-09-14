7c762003d8537bf31be6ea64f8e1637d753f54da
daveyshindig <davey.wilkie@gmail.com>
2020-09-13 23:00:06 -1000

    Adds HelloWorld.java

---
+++ src/com/e71/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.e71;
+
+import java.lang.reflect.Array;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        if (Array.getLength(args) == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.println("Hello, " + args[0] + "!");
+        }
+    }
+}
